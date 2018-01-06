0c5c2f4594c86f775799a97abc74b84ad98c0d8b
Jon Lambson <jonlambson@gmail.com>
2018-01-06 13:15:54 -0700

    hooked up reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 181 },
+    { title: 'Harry Potter', pages: 242 },
+    { title: 'The Dark Towers', pages: 343 },
+    { title: 'Eloquent Ruby', pages: 50 }
+  ]
+}
